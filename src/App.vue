cd399ca677b9a5905c1aa1c97402eb2aed0896fa
alexzhang1030 <1642114555@qq.com>
2022-12-29 19:48:31 +0800

    feat(ui): layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { NConfigProvider, darkTheme } from 'naive-ui';
+import { computed } from 'vue';
+import { isDark } from './composable/dark';
+
+const naiveTheme = computed(() => isDark.value ? darkTheme : null);
+</script>
 <template>
-  <div>
-    <RouterView></RouterView>
-  </div>
+  <NConfigProvider :theme="naiveTheme">
+    <div>
+      <RouterView></RouterView>
+    </div>
+  </NConfigProvider>
 </template>
 
 <style scoped>
+
 </style>
--- src/pages/Task.vue
+++ src/pages/Task.vue
@@ -1,14 +1,28 @@
 <template>
 	<div class="
-		w-full  bg-gray-400 text-black dark:bg-#111111 dark:text-white
+		w-full  bg-gray-100 text-black dark:bg-#18181c dark:text-white
 		h-40px px-1% flex justify-between items-center
 		text-16px
 	">
 		<div>Vue3 Todo App Real-World</div>
 		<TheHeader />
 	</div>
-	<div class="flex h-[calc(100vh-40px)] w-screen">
-		<div class="w-1/5 bg-gray-500">
+	<NLayout has-sider class="!h-[calc(100vh-40px)]">
+		<NLayoutSider collapse-mode="transform" :collapsed-width="0" :width="240" show-trigger="bar"
+			content-style="padding: 24px;" bordered>
+			<TaskLeftListView></TaskLeftListView>
+		</NLayoutSider>
+		<NLayoutContent>
+			<NLayoutContent content-style="padding: 24px">
+				<div class="flex w-full h-full">
+					<TaskList class="flex-1"></TaskList>
+					<TaskEditor class="flex-1"></TaskEditor>
+				</div>
+			</NLayoutContent>
+		</NLayoutContent>
+	</NLayout>
+	<!-- <div class="flex h-[calc(100vh-40px)] w-screen flex">
+		<div class="w-1/10 bg-gray-200 dark:bg-gray-800">
 			<TaskLeftListView></TaskLeftListView>
 		</div>
 		<div class="w-2/5 bg-pink-500">
@@ -17,7 +31,7 @@
 		<div class="w-2/5 bg-blue-500">
 			<TaskEditor></TaskEditor>
 		</div>
-	</div>
+	</div> -->
 </template>
 
 <script setup lang="ts">
@@ -25,4 +39,5 @@ import TaskList from '@/components/task/TaskList.vue'
 import TaskEditor from '@/components/task/TaskEditor.vue'
 import TaskLeftListView from '@/components/task/TaskLeftListView.vue'
 import TheHeader from '@/components/header/TheHeader.vue';
+import { NLayout, NLayoutContent, NLayoutSider } from 'naive-ui';
 </script>
