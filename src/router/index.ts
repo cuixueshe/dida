da59f94af47988bcd7b6bfd6b3b1ab95bd17a7ea
cuixiaorui <cui_xiaorui@126.com>
2022-12-29 23:34:50 +0800

    chore: cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,9 @@
 import type { RouteRecordRaw } from 'vue-router'
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import Home from '../pages/Home.vue'
-import About from '../pages/About.vue'
 import Task from '../pages/Task.vue'
 
 export const routes: RouteRecordRaw[] = [
-  // { path: "/", component: Home, name: "Home" },
   { path: '/', redirect: '/task' },
-  { path: '/about', component: About, name: 'About' },
   { path: '/task', component: Task, name: 'Task' },
 ]
 
