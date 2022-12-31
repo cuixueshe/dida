351bd7ecba3b558c1369e64996a9d2521dacf64a
凤之兮原 <1577310596@qq.com>
2022-12-31 17:20:01 +0800

    chore: add unocss shortcuts to LeftListView

--- unocss.config.ts
+++ unocss.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   shortcuts: [
     ['btn', ''],
     ['item-hover', ''],
+    ['li_common','cursor-pointer flex justify-between items-center h-7'],
   ],
   presets: [
     presetUno(),
