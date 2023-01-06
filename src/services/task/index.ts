5d5f1a99650c7b93d2c491f6eaf326c661ac4c4c
cuixiaorui <cui_xiaorui@126.com>
2023-01-06 22:09:16 +0800

    refactor: extract specialProject

--- src/services/task/index.ts
+++ src/services/task/index.ts
@@ -3,12 +3,13 @@ import { initProjects } from './project'
 export { TaskState, createTask, restoreTask } from './task'
 export {
   projects,
-  SpecialProjectNames,
   addTaskToCompleteProject,
   addTaskToProject,
   findProjectByName,
   removeTaskToTrashProject,
 } from './project'
+
+export { SpecialProjectNames } from './specialProject'
 export type { Task } from './task'
 export type { Project } from './project'
 
