f59f8c895307c689c34b0dff7f68f0929a7cea71
cuixiaorui <cui_xiaorui@126.com>
2023-01-08 16:40:59 +0800

    refactor: extract method

--- src/components/task/TaskItem.vue
+++ src/components/task/TaskItem.vue
@@ -11,13 +11,54 @@ interface Props {
 }
 
 const props = defineProps<Props>()
+const taskStore = useTaskStore()
 
-const { message } = createDiscreteApi(
-  ['message'],
-)
+function useMessage() {
+  const { message } = createDiscreteApi(['message'])
 
-let messageReactive: MessageReactive | null = null
+  let messageReactive: MessageReactive | null = null
 
+  function createMessageView(title: string, onClick: () => void) {
+    return () =>
+      h('p', null, [
+        h('span', null, `${title} ${SpecialProjectNames.Complete}`),
+        h(
+          'i',
+          {
+            style:
+              'color: teal;font-style:unset;cursor:pointer;margin-left: 20px',
+            onClick,
+          },
+          '撤销',
+        ),
+      ])
+  }
+
+  function removeMessage() {
+    if (messageReactive) {
+      messageReactive.destroy()
+      messageReactive = null
+    }
+  }
+
+  function showCompleteMessage(task: Task) {
+    const onClick = () => {
+      taskStore.restoreTask(task)
+      removeMessage()
+    }
+
+    messageReactive = message.info(createMessageView(task.title, onClick), {
+      icon: () => null,
+      duration: 1000,
+    })
+  }
+
+  return {
+    showCompleteMessage,
+  }
+}
+
+const { showCompleteMessage } = useMessage()
 const checkboxColors: Record<TaskState, string> = {
   [TaskState.ACTIVE]: 'bg-#ccc',
   [TaskState.COMPLETED]: 'bg-#007A78',
@@ -25,7 +66,6 @@ const checkboxColors: Record<TaskState, string> = {
   [TaskState.REMOVED]: 'bg-#ccc',
 }
 
-const taskStore = useTaskStore()
 const { showContextMenu } = useTaskRightContextMenu()
 
 function handleRightClickTask(e: MouseEvent, task: Task) {
@@ -45,7 +85,7 @@ function handleInput(e: Event) {
 function handleCompleteTodo(e: Event) {
   if (props.task.state === TaskState.ACTIVE) {
     taskStore.completeTask(props.task)
-    completeMessage(props.task)
+    showCompleteMessage(props.task)
   }
   else if (props.task.state === TaskState.COMPLETED) {
     taskStore.restoreTask(props.task)
@@ -55,37 +95,16 @@ function handleCompleteTodo(e: Event) {
     console.log('在垃圾桶里面的 task 不可以直接恢复')
   }
 }
-
-function completeMessage(task: Task) {
-  messageReactive = message.info(
-    () => h('p', null, [
-      h('span', null, `${task.title} ${SpecialProjectNames.Complete}`),
-      h('i', {
-        style: 'color: teal;font-style:unset;cursor:pointer;margin-left: 20px',
-        onClick: () => {
-          taskStore.restoreTask(task)
-          removeMessage()
-        },
-      }, '撤销'),
-    ]),
-    {
-      icon: () => null,
-      duration: 1000,
-    },
-  )
-}
-
-function removeMessage() {
-  if (messageReactive) {
-    messageReactive.destroy()
-    messageReactive = null
-  }
-}
 </script>
 
 <template>
-  <div class="flex flex-row gap-10px w-full items-center" @click.right="handleRightClickTask($event, task)">
-    <i class="cursor-move text-gray-200 dark:text-#3B3B3B flex-shrink-0 i-mdi-format-align-justify text-sm" />
+  <div
+    class="flex flex-row gap-10px w-full items-center"
+    @click.right="handleRightClickTask($event, task)"
+  >
+    <i
+      class="cursor-move text-gray-200 dark:text-#3B3B3B flex-shrink-0 i-mdi-format-align-justify text-sm"
+    />
     <div class="flex justify-start items-center gap-5px h-40px py-5px flex-1">
       <template v-if="task.state === TaskState.REMOVED">
         <!-- 临时加的提示 后面要去掉 -->
