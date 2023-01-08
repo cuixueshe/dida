584f4087bcc99a8817f87e2f50f0d6da9c6d7b6c
凤之兮原 <1577310596@qq.com>
2023-01-08 17:14:39 +0800

    feat: add useTaskStatusStore

---
+++ src/store/useTaskStatus.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useTaskStatusStore = defineStore(
+  'taskStatus',
+  () => {
+    const selectedKey = ref([101])
+    const listDefaultSelectedKey = ref([100])
+
+    function changeSelectedKey(key: number[]) {
+      console.log('changeSelectedKey', key)
+      selectedKey.value = key
+    }
+
+    return {
+      selectedKey,
+      listDefaultSelectedKey,
+      changeSelectedKey,
+    }
+  }
+)
