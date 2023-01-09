3414cbbe9cb15ff0e04795225991016907dfa6e9
alexzhang1030 <1642114555@qq.com>
2023-01-09 22:48:37 +0800

    feat(ui): settings body

---
+++ src/components/settings/SettingsBody.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { NLayout, NLayoutContent, NLayoutSider } from 'naive-ui'
+</script>
+
+<template>
+  <div class="flex justify-between items-center min-h-400px rounded-5px overflow-hidden">
+    <div class="w-1/5 dark:bg-#1f1f1f bg-white min-h-400px">
+      222
+    </div>
+    <div class="w-4/5 dark:bg-#2e2e2e bg-white min-h-400px" />
+  </div>
+</template>
---
+++ src/components/settings/SettingsHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { NSpace } from 'naive-ui'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const routeName = route.meta?.title
+
+const router = useRouter()
+
+const goToHome = () => {
+  router.push({
+    path: '/',
+  })
+}
+</script>
+
+<template>
+  <NSpace justify="space-between" item-style="center">
+    <h1 class="text-3xl">
+      {{ routeName }}
+    </h1>
+    <button
+      class="
+            w-100px h-40px rounded-md bg-blue-500 text-white text-16px
+            hover:bg-blue-400 active:bg-blue-700 transition-colors
+          "
+      @click="goToHome()"
+    >
+      完成
+    </button>
+  </NSpace>
+</template>
