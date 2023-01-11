302ec55ce0acb6bba33c67fda42264094f3ea94a
Eadweard <804875728@qq.com>
2023-01-11 18:09:45 +0800

    新增隐藏智能清单功能

--- src/components/task/TaskLeftListSmartProject.vue
+++ src/components/task/TaskLeftListSmartProject.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue'
 import { reactive } from 'vue'
+import { useProjectMoreActions } from '@/composable/useProjectMoreActions';
+import { NPopover } from "naive-ui"
 import {
   SmartProjectNames,
   useProjectSelectedStatusStore,
@@ -39,6 +41,7 @@ const selected = 'bg-[#E7F5EE] dark:bg-[#233633]'
 
 const taskStore = useTaskStore()
 const projectSelectedStatusStore = useProjectSelectedStatusStore()
+const {showMoreIconIndex, showWitchPopover, openPopover, hideTaskItem, canShowTaskList} = useProjectMoreActions()
 
 const handleTaskItemClick = (projectName: string, key: number) => {
   taskStore.changeCurrentActiveProject(projectName)
@@ -51,6 +54,7 @@ const handleTaskItemClick = (projectName: string, key: number) => {
     <li
       v-for="item in taskList"
       :key="item.key"
+      v-show="canShowTaskList.includes(item.key)"
       li_common
       pl-4
       pr-2
@@ -59,6 +63,8 @@ const handleTaskItemClick = (projectName: string, key: number) => {
         projectSelectedStatusStore.selectedKey[0] === item.key ? selected : ''
       "
       @click="handleTaskItemClick(item.title, item.key)"
+      @mouseenter="showMoreIconIndex = item.key"
+      @mouseleave="showMoreIconIndex = -1"
     >
       <div flex>
         <Icon
@@ -70,13 +76,23 @@ const handleTaskItemClick = (projectName: string, key: number) => {
         <span class="ml-2">{{ item.title }}</span>
       </div>
 
-      <Icon
-        v-show="projectSelectedStatusStore.selectedKey[0] === item.key"
-        icon="material-symbols:more-horiz"
-        width="20"
-        class="color-[#9D9FA3]"
-        dark="color-white"
-      />
+      <NPopover trigger="click" style="padding: 5px 0 5px 0" @clickoutside="showWitchPopover = -1" :show="showWitchPopover === item.key"
+           :show-arrow="false" placement="bottom-start">
+            <template #trigger>
+              <Icon
+                v-show="projectSelectedStatusStore.selectedKey[0] === item.key || showMoreIconIndex === item.key"
+                icon="material-symbols:more-horiz"
+                width="20"
+                class="color-[#9D9FA3]"
+                dark="color-white"
+                @click="($event) => {$event.stopPropagation(); openPopover(item.key)}"
+              />
+            </template>
+            <ul w-180px cursor-pointer>
+              <li hover="bg-[#F3F3F5] dark:bg-[#2D2D30]" pl-4 text-14px h-20px lh-20px
+              @click="hideTaskItem(item.key)">隐藏</li>
+            </ul>
+      </NPopover>
     </li>
   </ul>
 </template>
