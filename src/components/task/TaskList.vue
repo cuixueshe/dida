b38b1c314b9d4a665142ecf520f25b9bbcbdcb6c
HuiPeng Lu <72397995+lhp96@users.noreply.github.com>
2023-01-11 17:28:28 +0800

    fix: when chinese typing placeholderText hidden

--- src/components/task/TaskList.vue
+++ src/components/task/TaskList.vue
@@ -49,6 +49,10 @@ function toggleLeftMenu() {
   taskLeftMenuStatusStore.toggle()
 }
 
+function handleInputChange(event: any) {
+  taskTitle.value = event.target.value
+}
+
 const shouldShowTodoAdd = computed(() => {
   const name = taskStore.currentActiveProject?.name
   return (
@@ -85,9 +89,10 @@ const { inputRef, onFocus } = useInput()
     >
       <input
         ref="inputRef"
-        v-model="taskTitle"
+        :value="taskTitle"
         type="text"
         class="w-100% min-w-300px h-38px rounded-6px p-4px pl-12px pr-12px outline-none border-1 b-transparent bg-gray-100 dark:bg-#3B3B3B"
+        @input="handleInputChange"
         @keypress.enter="addTask"
       >
       <div
