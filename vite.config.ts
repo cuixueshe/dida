12eac16525b3d7d7fee268c1b34ddcc7d098d6e0
cuixiaorui <cui_xiaorui@126.com>
2023-01-12 18:20:24 +0800

    chore: add alias services/task

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
+      'services/': `${path.resolve(__dirname, 'src/services')}/`,
     },
   },
 })
