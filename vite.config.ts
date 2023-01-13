c5fc2b04df4ffdb85d370c516ec000c0ff75da9a
Nic_ <36131101+NicCraver@users.noreply.github.com>
2023-01-13 22:58:40 +0800

    feat: add unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +17,26 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: path.resolve(__dirname, './scripts/vitest.setup.ts'),
   },
-  plugins: [vueJsx(), vue(), Unocss()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    Unocss(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia', {
+        'naive-ui': [
+          'useDialog',
+          'useMessage',
+          'useNotification',
+          'useLoadingBar',
+        ],
+      }],
+      dts: 'typings/auto-imports.d.ts',
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: 'typings/components.d.ts',
+    }),
+  ],
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
