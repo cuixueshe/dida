389c28fc85b508651431df1606b495a7a32128af
alexzhang1030 <1642114555@qq.com>
2023-01-14 19:39:55 +0800

    feat: migrate to monorepo

--- apps/frontend/cypress.config.ts
+++ apps/frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress'
 
 const baseUrl
   = process.env.NODE_ENV === 'developer'
-    ? 'http://127.0.0.1:5174/#/'
+    ? 'http://127.0.0.1:5173/#/'
     : 'http://127.0.0.1:4173/#/'
 
 export default defineConfig({
