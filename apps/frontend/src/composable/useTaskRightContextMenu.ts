178591e34b8968ff62cfddffc0125ede82992cdd
cuixiaorui <cui_xiaorui@126.com>
2023-02-10 11:35:10 +0800

    fix: types

--- apps/frontend/src/composable/useTaskRightContextMenu.ts
+++ apps/frontend/src/composable/useTaskRightContextMenu.ts
@@ -7,8 +7,11 @@ import { useTaskStore } from '@/store'
 export function useTaskRightContextMenu() {
   const taskStore = useTaskStore()
   const { showRemoveMessage, showMoveMessage } = useTaskOperationMessage()
-  const moveProjects = [...getSearchMenuItem(), ...getMoveListProjects()]
-  const menuData = reactive< MenuOptions >({
+  const moveProjects: NonNullable<MenuOptions['items']> = [
+    ...getSearchMenuItem(),
+    ...getMoveListProjects(),
+  ]
+  const menuData = reactive<MenuOptions>({
     x: 0,
     y: 0,
     items: [
@@ -27,33 +30,38 @@ export function useTaskRightContextMenu() {
   })
 
   function getSearchMenuItem() {
-    return [{
-      label: h('input', {
-        style: {
-          width: '100px',
-          height: '20px',
-          outlineColor: 'rgb(86, 125, 250)',
-        },
-        placeholder: '搜索',
-        id: 'seaechInput',
-        onInput: changeInput,
-      }),
-      clickClose: false,
-    }]
+    return [
+      {
+        label: h('input', {
+          style: {
+            width: '100px',
+            height: '20px',
+            outlineColor: 'rgb(86, 125, 250)',
+          },
+          placeholder: '搜索',
+          id: 'seaechInput',
+          onInput: changeInput,
+        }),
+        clickClose: false,
+      },
+    ]
   }
 
   function changeInput() {
-    const inputValue = (<HTMLInputElement>document.getElementById('seaechInput')).value
-    if (!inputValue) {
-      menuData.items[0].children = moveProjects
-      return
+    const inputValue = (<HTMLInputElement>(
+      document.getElementById('seaechInput')
+    )).value
+    if (menuData.items) {
+      if (!inputValue) {
+        menuData.items[0].children = moveProjects
+        return
+      }
+      menuData.items[0].children = moveProjects.filter((projectItem) => {
+        if (typeof projectItem.label === 'string')
+          return (projectItem.label as string).includes(inputValue)
+        else return true
+      })
     }
-    menuData.items[0].children = moveProjects.filter((projectItem) => {
-      if (typeof projectItem.label === 'string')
-        return (projectItem.label as string).includes(inputValue)
-
-      else return true
-    })
   }
 
   function getMoveListProjects() {
@@ -86,7 +94,8 @@ export function useTaskRightContextMenu() {
     e.preventDefault()
     menuData.x = e.x
     menuData.y = e.y
-    menuData.items[0].children = moveProjects
+    if (menuData.items)
+      menuData.items[0].children = moveProjects
     ContextMenu.showContextMenu(menuData)
   }
 
