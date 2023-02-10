929b4f9662776d43eb716c01c45bf9ccc75f7329
张晓旭 <35422319+1172208932@users.noreply.github.com>
2023-02-10 09:57:42 +0800

    feat: add search task can be move to other project(#117) (#137)

--- apps/frontend/src/composable/useTaskRightContextMenu.ts
+++ apps/frontend/src/composable/useTaskRightContextMenu.ts
@@ -1,11 +1,60 @@
-import { h } from 'vue'
+import { h, reactive } from 'vue'
 import ContextMenu from '@imengyu/vue3-context-menu'
+import type { MenuOptions } from '@imengyu/vue3-context-menu/index'
 import { useTaskOperationMessage } from './useTaskOperationMessage'
 import { useTaskStore } from '@/store'
 
 export function useTaskRightContextMenu() {
   const taskStore = useTaskStore()
   const { showRemoveMessage, showMoveMessage } = useTaskOperationMessage()
+  const moveProjects = [...getSearchMenuItem(), ...getMoveListProjects()]
+  const menuData = reactive< MenuOptions >({
+    x: 0,
+    y: 0,
+    items: [
+      {
+        label: '移动到',
+        children: moveProjects,
+      },
+      {
+        label: '删除',
+        onClick: () => {
+          showRemoveMessage(taskStore.currentActiveTask!)
+          taskStore.removeTask(taskStore.currentActiveTask!)
+        },
+      },
+    ],
+  })
+
+  function getSearchMenuItem() {
+    return [{
+      label: h('input', {
+        style: {
+          width: '100px',
+          height: '20px',
+          outlineColor: 'rgb(86, 125, 250)',
+        },
+        placeholder: '搜索',
+        id: 'seaechInput',
+        onInput: changeInput,
+      }),
+      clickClose: false,
+    }]
+  }
+
+  function changeInput() {
+    const inputValue = (<HTMLInputElement>document.getElementById('seaechInput')).value
+    if (!inputValue) {
+      menuData.items[0].children = moveProjects
+      return
+    }
+    menuData.items[0].children = moveProjects.filter((projectItem) => {
+      if (typeof projectItem.label === 'string')
+        return (projectItem.label as string).includes(inputValue)
+
+      else return true
+    })
+  }
 
   function getMoveListProjects() {
     return taskStore.listProjects.map((projectItem) => {
@@ -35,23 +84,10 @@ export function useTaskRightContextMenu() {
 
   function showContextMenu(e: MouseEvent) {
     e.preventDefault()
-    ContextMenu.showContextMenu({
-      x: e.x,
-      y: e.y,
-      items: [
-        {
-          label: '移动到',
-          children: [...getMoveListProjects()],
-        },
-        {
-          label: '删除',
-          onClick: () => {
-            showRemoveMessage(taskStore.currentActiveTask!)
-            taskStore.removeTask(taskStore.currentActiveTask!)
-          },
-        },
-      ],
-    })
+    menuData.x = e.x
+    menuData.y = e.y
+    menuData.items[0].children = moveProjects
+    ContextMenu.showContextMenu(menuData)
   }
 
   return {
