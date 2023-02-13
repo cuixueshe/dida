0f5a7e40403b997bd6a076a85a5c6f146493fc0f
zhoulei <zhoulei@ewaresoft.com>
2023-02-13 17:06:15 +0800

    feat: the currently active project should be the newly created project(#133)

--- apps/frontend/src/components/task/TagView/ProjectCreateView/index.ts
+++ apps/frontend/src/components/task/TagView/ProjectCreateView/index.ts
@@ -1,6 +1,9 @@
 import { dialogInit } from '../helper'
 import ProjectCreatedView from './ProjectCreatedView.vue'
-
-export function projectCreatedViewModal() {
-  return dialogInit(ProjectCreatedView)
+import type { Project } from '@/services/task'
+interface ProjectCreatedViewOptions {
+  onOk?: (project: Project) => void
+}
+export function projectCreatedViewModal(options: ProjectCreatedViewOptions = {}) {
+  return dialogInit(ProjectCreatedView, options)
 }
