0e765266e2638c9e32ff1ac56644dd15a160ae1e
cuixiaorui <cui_xiaorui@126.com>
2023-06-04 12:21:03 +0800

    refactor: command view

--- apps/frontend/src/components/command/Command.vue
+++ apps/frontend/src/components/command/Command.vue
@@ -2,28 +2,35 @@
 import { NModal } from 'naive-ui'
 import { provide, ref } from 'vue'
 import CommandBody from './CommandBody.vue'
+import { useCommand } from './command'
 
-const showCommand = ref(false)
+// const showCommand = ref(false)
 const commandBody = ref<InstanceType<typeof CommandBody>>()
 
+const { show, registerKeyboardShortcut } = useCommand()
+registerKeyboardShortcut()
+
 provide('closeModal', () => [
-  showCommand.value = false,
+  // showCommand.value = false,
+  show.value = false,
   commandBody.value?.reset(),
 ])
 
 defineExpose({
   show() {
-    showCommand.value = true
+    // showCommand.value = true
+    show.value = true
   },
 })
 const updateShow = () => {
-  if (!showCommand.value)
+  // if (!showCommand.value)
+  if (!show.value)
     commandBody.value?.reset()
 }
 </script>
 
 <template>
-  <NModal v-model:show="showCommand" display-directive="show" @update:show="updateShow">
+  <NModal v-model:show="show" display-directive="show" @update:show="updateShow">
     <CommandBody ref="commandBody" />
   </NModal>
 </template>
--- apps/frontend/src/components/navbar/Navbar.vue
+++ apps/frontend/src/components/navbar/Navbar.vue
@@ -1,38 +1,49 @@
 <script setup lang="ts">
 import { NDropdown, NPopover } from 'naive-ui'
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import type { DropdownMixedOption } from 'naive-ui/es/dropdown/src/interface'
+import { useCommand } from '../command/command'
 import Command from '@/components/command/Command.vue'
-import { useIsMac } from '@/composables'
+import { useGoto } from '@/composables'
 
-const commandRef = ref<InstanceType<typeof Command>>()
+const { showCommand } = useCommand()
+const { gotoSettings } = useGoto()
 
-const isMac = useIsMac()
-
-// Command + K Or Command + / will show command in MacOS
-// Ctrl + K Or Ctrl + / in Windows
-window.addEventListener('keydown', (e: KeyboardEvent) => {
-  if ((e.key === 'k' || e.key === 'л') && (isMac.value ? e.metaKey : e.ctrlKey)) {
-    e.preventDefault()
-    commandRef.value?.show()
-  }
-  if ((e.key === '/' || e.key === ',') && (isMac.value ? e.metaKey : e.ctrlKey)) {
-    e.preventDefault()
-    commandRef.value?.show()
-  }
-})
-const router = useRouter()
-
-const settingOptions = [
+const settingOptions: DropdownMixedOption[] = [
   {
     label: '设置',
     key: 'settings',
     props: {
       onClick: () => {
-        router.push({
-          name: 'Settings',
-        })
+        gotoSettings()
       },
+    },
+  },
+]
+
+interface ActivityIconOption {
+  label: string
+  icon: string
+  onClick?: () => void
+}
+
+const activityIconOptions: ActivityIconOption[] = [
+  {
+    label: '任务',
+    icon: 'carbon-checkbox-checked-filled',
+  },
+  {
+    label: '日历',
+    icon: 'carbon-calendar-heat-map',
+  },
+  {
+    label: '打卡',
+    icon: 'carbon-task-complete',
+  },
+  {
+    label: '搜索面板',
+    icon: 'carbon-search',
+    onClick() {
+      showCommand()
     },
   },
 ]
@@ -40,10 +51,7 @@ const settingOptions = [
 
 <template>
   <div class="w-[48px] border-r border-blue border-solid py-2 dark:bg-black">
-    <NDropdown
-      placement="right-end"
-      :options="settingOptions"
-    >
+    <NDropdown placement="right-end" :options="settingOptions">
       <!-- avatar -->
       <div class="w-[32px] h-[32px] rounded-sm bg-blue mx-auto">
         <img src="" alt="">
@@ -53,44 +61,19 @@ const settingOptions = [
     <div
       class="text-gray-4 flex flex-col justify-center items-center text-xl mt-10 gap-6 font-bold"
     >
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-            class="hover:text-gray-6 dark:hover:text-gray-2"
-            i="carbon-checkbox-checked-filled"
-          />
-        </template>
-        <span>任务</span>
-      </NPopover>
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-            class="hover:text-gray-6 dark:hover:text-gray-2"
-            i="carbon-calendar-heat-map"
-          />
-        </template>
-        <span>日历</span>
-      </NPopover>
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-            class="hover:text-gray-6 dark:hover:text-gray-2"
-            i="carbon-task-complete"
-          />
-        </template>
-        <span>打卡</span>
-      </NPopover>
-      <NPopover placement="right">
-        <template #trigger>
-          <div
-            class="hover:text-gray-6 dark:hover:text-gray-2 cursor-pointer"
-            i="carbon-search"
-            @click="commandRef?.show()"
-          />
-        </template>
-        <span>搜索面板</span>
-      </NPopover>
+      <template v-for="(iconOption, i) in activityIconOptions" :key="i">
+        <NPopover placement="right">
+          <template #trigger>
+            <div
+              class="hover:text-gray-6 dark:hover:text-gray-2"
+              :i="iconOption.icon"
+              @click="() => iconOption.onClick && iconOption.onClick()"
+            />
+          </template>
+          <span>{{ iconOption.label }}</span>
+        </NPopover>
+      </template>
     </div>
-    <Command ref="commandRef" />
+    <Command />
   </div>
 </template>
