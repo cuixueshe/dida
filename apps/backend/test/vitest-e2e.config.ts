cc92e5e71e4fcbdda15daf195e5aa85a928d4eff
cuixiaorui <cui_xiaorui@126.com>
2023-06-10 12:24:30 +0800

    test: delete backend test

--- apps/backend/test/vitest-e2e.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-import { mergeConfig } from 'vite'
-import defaultConfig from '../vitest.config'
-
-export default mergeConfig(defaultConfig, defineConfig({
-  test: {
-    include: ['**/*.e2e-spec.ts'],
-  },
-}))
