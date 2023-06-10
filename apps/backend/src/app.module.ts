440342c90ba6ef033ebcc15a80c33fc554ead325
cuixiaorui <cui_xiaorui@126.com>
2023-06-10 11:17:03 +0800

    feat: support docker start backend server

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TasksModule } from './tasks/tasks.module'
@@ -7,11 +8,16 @@ import { ProjectsModule } from './projects/projects.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development',
+      isGlobal: true,
+    }),
     ProjectsModule,
     TasksModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/dida'),
+    MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
