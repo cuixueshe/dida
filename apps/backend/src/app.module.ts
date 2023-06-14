2b2c587152c8178543422af90f214f9392d755f2
cuixiaorui <cui_xiaorui@126.com>
2023-06-14 13:43:27 +0800

    feat(back-end): unify the returned data format

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TasksModule } from './tasks/tasks.module'
 import { ProjectsModule } from './projects/projects.module'
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter'
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor'
 
 @Module({
   imports: [
@@ -17,7 +20,16 @@ import { ProjectsModule } from './projects/projects.module'
     MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    AppService],
 })
 
 export class AppModule {}
