2c9e54492b2200df806f6817579ee14a406c339d
cuixiaorui <cui_xiaorui@126.com>
2023-06-14 13:43:27 +0800

    feat(back-end): unify the returned data format

---
+++ apps/backend/src/core/filter/http-exception/http-exception.filter.ts
@@ -0,0 +1,24 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common'
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp()
+    const response = ctx.getResponse()
+    const status = exception.getStatus()
+
+    const message = exception.message
+      ? exception.message
+      : `${status >= 500 ? 'Service Error' : 'Client Error'}`
+
+    const errorResponse = {
+      data: {},
+      message,
+      code: -1,
+    }
+
+    response.status(status)
+    response.header('Content-Type', 'application/json; charset=utf-8')
+    response.send(errorResponse)
+  }
+}
