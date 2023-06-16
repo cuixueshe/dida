854f527b912855e1d8191a832fb2f397be3e1ae6
cuixiaorui <cui_xiaorui@126.com>
2023-06-16 23:06:33 +0800

    feat(teach-frontend): 04&05-测试 useRouter 的最佳测试&重构test helper

--- apps/teach-frontend/vite.config.ts
+++ apps/teach-frontend/vite.config.ts
@@ -1,10 +1,14 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
+  test: {
+    environment: 'happy-dom',
+    setupFiles: './vitest.setup.ts',
+  },
   server: {
     proxy: {
       '/api': {
---
+++ apps/teach-frontend/vitest.setup.ts
@@ -0,0 +1,25 @@
+import {
+  VueRouterMock,
+  createRouterMock,
+  injectRouterMock,
+} from 'vue-router-mock'
+import { beforeEach, vi } from 'vitest'
+import { config } from '@vue/test-utils'
+
+function setupRouterMock() {
+  const router = createRouterMock({
+    spy: {
+      create: fn => vi.fn(fn),
+      reset: spy => spy.mockClear(),
+    },
+  })
+
+  beforeEach(() => {
+    router.reset()
+    injectRouterMock(router)
+  })
+
+  config.plugins.VueWrapper.install(VueRouterMock)
+}
+
+setupRouterMock()
