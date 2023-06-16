de0b43aa0cc528de9f2d6221329a8a5b6599f8cd
cuixiaorui <cui_xiaorui@126.com>
2023-06-16 23:07:05 +0800

    refactor: test setup router mock

--- apps/frontend/vitest.setup.ts
+++ apps/frontend/vitest.setup.ts
@@ -6,15 +6,20 @@ import {
 } from 'vue-router-mock'
 import { beforeEach, vi } from 'vitest'
 
-// init vue-router
-const router = createRouterMock({
-  spy: {
-    create: fn => vi.fn(fn),
-    reset: spy => spy.mockReset(),
-  },
-})
-beforeEach(() => {
-  injectRouterMock(router)
-})
+function setupRouterMock() {
+  const router = createRouterMock({
+    spy: {
+      create: fn => vi.fn(fn),
+      reset: spy => spy.mockClear(),
+    },
+  })
 
-config.plugins.VueWrapper.install(VueRouterMock)
+  beforeEach(() => {
+    router.reset()
+    injectRouterMock(router)
+  })
+
+  config.plugins.VueWrapper.install(VueRouterMock)
+}
+
+setupRouterMock()
