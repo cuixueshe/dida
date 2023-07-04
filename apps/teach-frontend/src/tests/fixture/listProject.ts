dca3ee0b8acd63e3c6608ebfae8695859a332434
cuixiaorui <cui_xiaorui@126.com>
2023-07-04 08:47:41 +0800

    feat(teach-frontend): 10-测试 searchTasks & 测试 pinia

---
+++ apps/teach-frontend/src/tests/fixture/listProject.ts
@@ -0,0 +1,8 @@
+import type { ListProject } from '@/store'
+import { TasksSelectorType } from '@/store'
+
+export const liveListProject: ListProject = {
+  id: '1',
+  name: '生活',
+  type: TasksSelectorType.listProject,
+}
