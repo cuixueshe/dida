b9b3fbdd22dfd3ea5432d00faf0dc9640a54dca2
cuixiaorui <cui_xiaorui@126.com>
2023-07-11 21:04:51 +0800

    fix: cancel complete task bug

--- apps/teach-frontend/src/store/tasks.ts
+++ apps/teach-frontend/src/store/tasks.ts
@@ -82,6 +82,13 @@ export const useTasksStore = defineStore('tasksStore', () => {
 
   async function cancelCompleteTask(task: Task) {
     function taskPositionRestorer(task: Task) {
+      // only one task
+      if (tasks.value.length === 0) {
+        tasks.value.push(task)
+        return
+      }
+
+      // add to last position
       const lastTask = tasks.value[tasks.value.length - 1]
       if (task.position < lastTask.position) {
         tasks.value.push(task)
@@ -168,8 +175,8 @@ export const useTasksStore = defineStore('tasksStore', () => {
 
     updateTasks,
     changeActiveTask,
-    cancelCompleteTask,
     findAllTasksNotRemoved,
+    cancelCompleteTask,
     updateTaskTitle,
     updateTaskContent,
     updateTaskPosition,
