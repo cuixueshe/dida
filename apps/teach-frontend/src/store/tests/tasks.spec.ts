2d272d57b67cdc4026b5bf2d4b9602bfb01d1d48
cuixiaorui <cui_xiaorui@126.com>
2023-07-12 20:53:44 +0800

    feat(teach-frontend): 15-测试 tasks store(3)

--- apps/teach-frontend/src/store/tests/tasks.spec.ts
+++ apps/teach-frontend/src/store/tests/tasks.spec.ts
@@ -4,11 +4,18 @@ import { TaskStatus, useTasksStore } from '../tasks'
 import { useTasksSelectorStore } from '../tasksSelector'
 import { completeSmartProject } from '../smartProjects'
 import { liveListProject } from '@/tests/fixture'
-import { fetchCompleteTask, fetchCreateTask, fetchMoveTaskToProject, fetchRemoveTask, fetchRestoreTask } from '@/api'
+import {
+  fetchAllTasks,
+  fetchCompleteTask,
+  fetchCreateTask,
+  fetchMoveTaskToProject,
+  fetchRemoveTask,
+  fetchRestoreTask,
+} from '@/api'
 
 vi.mock('@/api')
 
-vi.mocked(fetchCreateTask).mockImplementation(async (title: string) => {
+function createTaskResponse(title: string) {
   return {
     title,
     content: '这是一个内容',
@@ -19,7 +26,14 @@ vi.mocked(fetchCreateTask).mockImplementation(async (title: string) => {
     createdAt: new Date().toString(),
     updatedAt: new Date().toString(),
   }
-})
+}
+
+vi.mocked(fetchCreateTask).mockImplementation(async (title: string) =>
+  createTaskResponse(title),
+)
+
+// vi.mocked(fetchAllTasks).mockResolvedValue([createTaskResponse('吃饭')])
+vi.mocked(fetchAllTasks).mockResolvedValue([])
 
 describe('tasks store', () => {
   beforeEach(() => {
@@ -116,4 +130,53 @@ describe('tasks store', () => {
     expect(tasksStore.currentActiveTask).toBeUndefined()
     expect(fetchMoveTaskToProject).toBeCalledWith(task.id, projectId)
   })
+
+  it('should update tasks ', async () => {
+    const tasksStore = useTasksStore()
+
+    tasksStore.updateTasks([createTaskResponse('吃饭')])
+
+    expect(tasksStore.tasks.length).toBe(1)
+    // expectTaskDataStructure(tasksStore.tasks[0])
+  })
+
+  describe('change active', () => {
+    it('should change active task by id', async () => {
+      const tasksStore = useTasksStore()
+      const task = (await tasksStore.addTask('吃饭'))!
+
+      tasksStore.changeActiveTask(task.id)
+
+      expect(tasksStore.currentActiveTask).toEqual(task)
+    })
+
+    it('should change active task by task', async () => {
+      const tasksStore = useTasksStore()
+      const task = (await tasksStore.addTask('吃饭'))!
+
+      tasksStore.changeActiveTask(task)
+
+      expect(tasksStore.currentActiveTask).toEqual(task)
+    })
+  })
+
+  it('should find all tasks not removed', async () => {
+    const tasksStore = useTasksStore()
+
+    await tasksStore.findAllTasksNotRemoved()
+
+    expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.ACTIVE })
+    expect(fetchAllTasks).toBeCalledWith({ status: TaskStatus.COMPLETED })
+
+    // expectTaskDataStructure(tasks[0])
+  })
 })
+
+// function expectTaskDataStructure(task: any) {
+//   expect(task.title).toBe('吃饭')
+//   expect(task).toHaveProperty('id')
+//   expect(task).toHaveProperty('content')
+//   expect(task).toHaveProperty('status')
+//   expect(task).toHaveProperty('position')
+//   expect(task).toHaveProperty('projectId')
+// }
