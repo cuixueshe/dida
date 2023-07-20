9a47153c28962ec8b3bc761ad531d856775359f1
cuixiaorui <cui_xiaorui@126.com>
2023-07-20 20:02:18 +0800

    refactor(backend): signin and signup

--- apps/backend/src/users/dto/signup-user.dto.ts
+++ apps/backend/src/users/dto/signup-user.dto.ts
@@ -1,19 +1,15 @@
-import { Equals, IsNotEmpty, IsString, Length } from 'class-validator'
+import { IsString, Length } from 'class-validator'
 
 export class SignupUserDto {
   @IsString()
-  @IsNotEmpty()
   @Length(6, 30)
   username: string
 
   @IsString()
-  @IsNotEmpty()
   @Length(6, 30)
   password: string
 
   @IsString()
-  @IsNotEmpty()
   @Length(6, 30)
-  @Equals('password', { message: '确认密码必须和密码一致' })
   confirmPassword: string
 }
--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,12 +1,11 @@
 import {
   Body,
   Controller,
+  HttpException,
   Inject,
   Post,
-  Res,
   ValidationPipe,
 } from '@nestjs/common'
-import { Response } from 'express'
 import { JwtService } from '@nestjs/jwt'
 import { UsersService } from './users.service'
 import { SignupUserDto } from './dto/signup-user.dto'
@@ -20,26 +19,51 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('signin')
-  async signin(@Body(ValidationPipe) signinUserDto: SigninUserDto, @Res({ passthrough: true }) res: Response) {
+  async signin(@Body(ValidationPipe) signinUserDto: SigninUserDto) {
     const user = await this.usersService.signin(signinUserDto)
 
     if (user) {
-      const token = await this.jwtService.signAsync({
-        user: {
-          id: user.id,
-          username: user.username,
-        },
-      })
-
-      res.setHeader('authorization', `bearer ${token}`)
-
-      return '登录成功'
+      const token = await this.signToken(user)
+      return this.mapResponse(user, token)
     }
-    else { return '登录失败' }
+    else {
+      throw new HttpException('登录失败', 200)
+    }
   }
 
   @Post('signup')
   async signup(@Body(ValidationPipe) signupUserDto: SignupUserDto) {
-    return await this.usersService.signup(signupUserDto)
+    if (signupUserDto.password !== signupUserDto.confirmPassword)
+      throw new HttpException('两次输入的密码不一致', 200)
+
+    const user = await this.usersService.signup(signupUserDto)
+
+    if (user) {
+      const token = await this.signToken(user)
+      return this.mapResponse(user, token)
+    }
+    else {
+      throw new HttpException('注册失败', 200)
+    }
+  }
+
+  private async signToken(user) {
+    const token = await this.jwtService.signAsync({
+      user: {
+        id: user.id,
+        username: user.username,
+      },
+    })
+
+    return token
+  }
+
+  private mapResponse(user, token) {
+    return {
+      user: {
+        username: user.username,
+      },
+      token,
+    }
   }
 }
