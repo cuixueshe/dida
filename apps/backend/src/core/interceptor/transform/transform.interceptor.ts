0bc51626e488313ffd41410990f6dfe266df8394
cuixiaorui <cui_xiaorui@126.com>
2023-07-25 16:08:40 +0800

    fix: msg to message

--- apps/backend/src/core/interceptor/transform/transform.interceptor.ts
+++ apps/backend/src/core/interceptor/transform/transform.interceptor.ts
@@ -9,7 +9,7 @@ export class TransformInterceptor implements NestInterceptor {
         return {
           data,
           code: 0,
-          msg: 'Request successful',
+          message: 'Request successful',
         }
       }),
     )
