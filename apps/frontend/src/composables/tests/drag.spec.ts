5bab2f28a4a7db9b3fd27b7d841b52c45b84a76d
cuixiaorui <cui_xiaorui@126.com>
2023-08-06 22:20:08 +0800

    refactor: task drag

---
+++ apps/frontend/src/composables/tests/drag.spec.ts
@@ -0,0 +1,95 @@
+import { afterEach, describe, expect, it, vi } from 'vitest'
+import { useDrag } from '../drag'
+import { fireEvent } from '@/tests/helper'
+
+describe('drag', () => {
+  afterEach(() => {
+    fireEvent.mouseup(document)
+  })
+  describe('basic', () => {
+    it('should moveDistance < 0 when moving to the left', () => {
+      const el = document.createElement('div')
+      const moveSpy = vi.fn()
+
+      useDrag({
+        el,
+        onMove: moveSpy,
+      })
+
+      fireEvent.mousedown(el, { clientX: 2 })
+      fireEvent.mousemove(document, { clientX: 1 })
+
+      expect(moveSpy).toBeCalledWith(-1)
+    })
+
+    it('move twice', () => {
+      const el = document.createElement('div')
+      const moveSpy = vi.fn()
+
+      useDrag({
+        el,
+        onMove: moveSpy,
+      })
+
+      fireEvent.mousedown(el, { clientX: 3 })
+      fireEvent.mousemove(document, { clientX: 2 })
+      fireEvent.mousemove(document, { clientX: 1 })
+
+      expect(moveSpy).toHaveBeenNthCalledWith(1, -1)
+      expect(moveSpy).toHaveBeenNthCalledWith(2, -1)
+    })
+
+    it('should moveDistance > 0 when moving to the right', () => {
+      const el = document.createElement('div')
+      const moveSpy = vi.fn()
+
+      useDrag({
+        el,
+        onMove: moveSpy,
+      })
+
+      fireEvent.mousedown(el, { clientX: 1 })
+      fireEvent.mousemove(document, { clientX: 2 })
+
+      expect(moveSpy).toBeCalledWith(1)
+    })
+  })
+
+  describe('move range', () => {
+    it('should not move when you are on the left edge', () => {
+      const el = document.createElement('div')
+      el.offsetLeft = 10
+
+      const moveSpy = vi.fn()
+
+      useDrag({
+        el,
+        moveRange: [10, 50],
+        onMove: moveSpy,
+      })
+
+      fireEvent.mousedown(el, { clientX: 10 })
+      fireEvent.mousemove(document, { clientX: 9 })
+
+      expect(moveSpy).not.toBeCalled()
+    })
+
+    it('should not move when you are on the right edge', () => {
+      const el = document.createElement('div')
+      el.offsetLeft = 50
+
+      const moveSpy = vi.fn()
+
+      useDrag({
+        el,
+        moveRange: [10, 50],
+        onMove: moveSpy,
+      })
+
+      fireEvent.mousedown(el, { clientX: 50 })
+      fireEvent.mousemove(document, { clientX: 51 })
+
+      expect(moveSpy).not.toBeCalled()
+    })
+  })
+})
--- apps/frontend/src/tests/helper/fireEvent.ts
+++ apps/frontend/src/tests/helper/fireEvent.ts
@@ -1,6 +1,23 @@
+type Node = HTMLElement | Document
+
 export const fireEvent = {
   keyDown(options: KeyboardEventInit) {
     const event = new KeyboardEvent('keydown', options)
     window.dispatchEvent(event)
+  },
+
+  mousedown(node: Node, options: MouseEventInit) {
+    const event = new MouseEvent('mousedown', options)
+    node.dispatchEvent(event)
+  },
+
+  mousemove(node: Node, options: MouseEventInit) {
+    const event = new MouseEvent('mousemove', options)
+    node.dispatchEvent(event)
+  },
+
+  mouseup(node: Node, options?: MouseEventInit) {
+    const event = new MouseEvent('mouseup', options)
+    node.dispatchEvent(event)
   },
 }
